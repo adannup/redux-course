e4214aca688d79dbed5a86e8c048b472831b034f
adannup <zad3life@gmail.com>
2018-02-22 00:07:04 -0600

    Add Counter example

--- react/src/js/index.js
+++ react/src/js/index.js
@@ -1,19 +1,50 @@
+// This example was made from here: https://egghead.io/lessons/react-redux-react-counter-example
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { AppContainer } from 'react-hot-loader';
-import App from './App';
+import { createStore } from 'redux';
+import Counter from './Counter';
 
-const render = Component => {
-  ReactDOM.render(
-    <AppContainer>
-      <Component />
-    </AppContainer>,
-    document.getElementById('root')
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+    default:
+      return state;
+  }
+}
+
+const store = createStore(counter);
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <h1> Hola mundo test</h1>
+      <Counter
+        value={store.getState()}
+        onIncrement={() =>
+          store.dispatch({
+            type: 'INCREMENT',
+          })
+        }
+        onDecrement={() =>
+          store.dispatch({
+            type: 'DECREMENT',
+          })
+        }
+      />
+    </React.Fragment>
   );
 }
 
-render(App);
-// Webpack Hot Module Replacement API
-if (module.hot) {
-  module.hot.accept('./App', () => { render(App) })
-}
+const render = () => {
+  ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+  );
+};
+
+store.subscribe(render);
+render();
